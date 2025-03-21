e45eb5c44d9a09fd256cf50961a9c5bf5c8693e9
김창민 <changmin@gimchangmin-ui-MacBookPro.local>
2025-03-21 19:54:58 +0900

    add: 네이버 check style rules 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.22" // Kotlin JVM 버전을 Java 21에 맞춤
+    kotlin("jvm") version "1.9.22"
     id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
 }
@@ -14,14 +14,30 @@ subprojects {
     apply(plugin = "java")
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "checkstyle")
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(21)) // Java 21 설정
+            languageVersion.set(JavaLanguageVersion.of(21))
         }
     }
 
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter-web")
+    }
+
+    configure<CheckstyleExtension> {
+        toolVersion = "10.12.1"
+        configFile = rootProject.file("config/checkstyle/naver-checkstyle-rules.xml")
+        configProperties = mapOf("suppressionFile" to "${rootDir}/config/checkstyle/naver-checkstyle-suppressions.xml")
+        isIgnoreFailures = false
+        isShowViolations = true
+    }
+
+    tasks.withType<Checkstyle> {
+        reports {
+            xml.required.set(false)
+            html.required.set(true)
+        }
     }
 }
