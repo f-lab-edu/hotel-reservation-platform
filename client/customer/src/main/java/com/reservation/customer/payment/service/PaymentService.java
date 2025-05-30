a7753c47abde2c8cc094a691a4a399e1210aeb0f
김창민 <changmin@gimchangmin-ui-MacBookPro.local>
2025-05-30 18:23:25 +0900

    add: 예약 생성 및 취소 처리 과정에서 redisson 기반 비관적 락 활용

--- client/customer/src/main/java/com/reservation/customer/payment/service/PaymentService.java
+++ client/customer/src/main/java/com/reservation/customer/payment/service/PaymentService.java
@@ -4,7 +4,10 @@ import static com.reservation.support.utils.retry.OptimisticLockingFailureRetryU
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,13 +30,20 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
+	private static final int MAX_OPTIMISTIC_RETRY_COUNT = 5;
+	private static final long MAX_LOCK_WAIT_TIME_SECONDS = 10L; // 락 대기 최대 시간
+	private static final long LOCK_WAIT_TIME_SECONDS = 5L;
+
 	private final Iamport iamport;
 	private final JpaPaymentRepository paymentRepository;
 	private final JpaReservationRepository reservationRepository;
 	private final JpaRoomAvailabilityRepository roomAvailabilityRepository;
 
+	private final RedissonClient redisson;
+
+	// 결제 검증 과정에서 낙관적 락을 사용하여 예약 가능 수량을 증가시키는 방식
 	@Transactional
-	public IamportPayment validationPayment(String paymentUid, long reservationId) {
+	public IamportPayment optimisticValidationPayment(String paymentUid, long reservationId) {
 		IamportPayment iamportPayment;
 
 		iamportPayment = new IamportPayment(iamport.paymentByImpUid(paymentUid));
@@ -63,27 +73,27 @@ public class PaymentService {
 			// 결제금액 위변조로 의심되는 결제금액을 취소(아임포트)
 			payment.markCancelled();
 			paymentRepository.save(payment);
-			return cancelPayment(reservation, iamportPayment.payment().getImpUid(), iamportPrice);
+			return optimisticCancelPayment(reservation, iamportPayment.payment().getImpUid(), iamportPrice);
 		}
 
 		// 결제 완료
 		payment.markCompleted();
 		paymentRepository.save(payment);
-		
+
 		// 예약 완료
 		reservation.markConfirmed();
 		reservationRepository.save(reservation);
 		return iamportPayment;
 	}
 
-	// 아임포트 결제 취소 처리
-	private IamportPayment cancelPayment(Reservation reservation, String paymentUid, int amount) {
+	// 아임포트 결제 취소 처리 -> 낙관적 락 기반
+	private IamportPayment optimisticCancelPayment(Reservation reservation, String paymentUid, int amount) {
 		// 1. 예약 취소
 		reservation.markCanceled();
 		reservationRepository.save(reservation);
 
 		// 2. 예약 가능 수량 증가 (낙관적 락 기반)
-		executeWithRetry(5, () -> increaseAvailabilityCount(reservation));
+		executeWithRetry(MAX_OPTIMISTIC_RETRY_COUNT, () -> increaseAvailabilityCount(reservation));
 
 		// 3. 결제 취소
 		CancelData cancelData = new CancelData(paymentUid, true, new BigDecimal(amount));
@@ -103,5 +113,94 @@ public class PaymentService {
 
 		// 버전 기반 낙관적 락으로 saveAll 시점에 충돌 검증
 		return roomAvailabilityRepository.saveAll(availabilities);
+	}
+
+	// 결제 검증 과정에서 레디슨 락을 사용하여 예약 가능 수량을 증가시키는 방식
+	@Transactional
+	public IamportPayment redissonValidationPayment(String paymentUid, long reservationId) {
+		IamportPayment iamportPayment;
+
+		iamportPayment = new IamportPayment(iamport.paymentByImpUid(paymentUid));
+
+		// 결제 완료가 아니면
+		if (!iamportPayment.payment().getStatus().equals("paid")) {
+			throw ErrorCode.CONFLICT.exception("결제 미완료 상태입니다. 다시 결제해주세요");
+		}
+
+		// 예약 조회
+		Reservation reservation = reservationRepository.findById(reservationId)
+			.orElseThrow(() -> ErrorCode.CONFLICT.exception("예약된 내역이 없습니다."));
+
+		// 결제해야할 예약 금액
+		int reservationTotalPrice = reservation.getTotalPrice();
+		// 실 결제 금액
+		int iamportPrice = iamportPayment.payment().getAmount().intValue();
+
+		Payment payment = Payment.builder()
+			.paymentUid(paymentUid)
+			.price(iamportPrice)
+			.status(PaymentStatus.INITIATED)
+			.build();
+
+		// 결제 금액 검증
+		if (iamportPrice != reservationTotalPrice) {
+			// 결제금액 위변조로 의심되는 결제금액을 취소(아임포트)
+			payment.markCancelled();
+			paymentRepository.save(payment);
+			return redissonCancelPayment(reservation, iamportPayment.payment().getImpUid(), iamportPrice);
+		}
+
+		// 결제 완료
+		payment.markCompleted();
+		paymentRepository.save(payment);
+
+		// 예약 완료
+		reservation.markConfirmed();
+		reservationRepository.save(reservation);
+		return iamportPayment;
+	}
+
+	// 아임포트 결제 취소 처리 -> 레디슨 락 기반
+	private IamportPayment redissonCancelPayment(Reservation reservation, String paymentUid, int amount) {
+		// 1. 예약 취소
+		reservation.markCanceled();
+		reservationRepository.save(reservation);
+
+		// 2. 비관적 락 처리 시작
+		RLock lock = getReservationLock(reservation);
+
+		boolean isLocked = false;
+		try {
+			isLocked = lock.tryLock(MAX_LOCK_WAIT_TIME_SECONDS, LOCK_WAIT_TIME_SECONDS, TimeUnit.SECONDS);
+
+			if (!isLocked) {
+				throw ErrorCode.CONFLICT.exception("해당 객실은 현재 다른 예약 처리 중입니다. 잠시 후 다시 시도해 주세요.");
+			}
+
+			// 2. 예약 가능 수량 증가
+			increaseAvailabilityCount(reservation);
+
+			// 3. 결제 취소
+			CancelData cancelData = new CancelData(paymentUid, true, new BigDecimal(amount));
+			return new IamportPayment(iamport.cancelPaymentByImpUid(cancelData));
+
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			throw ErrorCode.INTERNAL_SERVER_ERROR.exception("예약 처리 중 오류 발생");
+		} finally {
+			if (isLocked) {
+				lock.unlock();
+			}
+		}
+	}
+
+	private RLock getReservationLock(Reservation reservation) {
+		String lockKey =
+			"reservation:lock:"
+				+ reservation.getRoomTypeId()
+				+ ":" + reservation.getCheckIn() + ":"
+				+ reservation.getCheckOut();
+
+		return redisson.getLock(lockKey);
 	}
 }
--- client/customer/src/main/java/com/reservation/customer/reservation/service/ReservationService.java
+++ client/customer/src/main/java/com/reservation/customer/reservation/service/ReservationService.java
@@ -4,7 +4,10 @@ import static com.reservation.support.utils.retry.OptimisticLockingFailureRetryU
 
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,14 +26,21 @@ import com.reservation.domain.roomtype.RoomType;
 import com.reservation.support.exception.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
+	private static final int MAX_OPTIMISTIC_RETRY_COUNT = 5;
+	private static final long MAX_LOCK_WAIT_TIME_SECONDS = 10L; // 락 대기 최대 시간
+	private static final long LOCK_WAIT_TIME_SECONDS = 5L;
+
 	private final JpaReservationRepository reservationRepository;
 	private final JpaMemberRepository memberRepository;
 	private final JpaRoomAvailabilityRepository roomAvailabilityRepository;
 	private final JpaRoomTypeRepository roomTypeRepository;
+	private final RedissonClient redisson;
 
 	@Value("${iamport.imp-uid}")
 	private String impUid;
@@ -57,7 +67,7 @@ public class ReservationService {
 		validateAvailability(command);
 
 		// 3. 수량 차감 (낙관적 락 기반) 5회 retry
-		executeWithRetry(5, () -> decreaseAvailabilityCount(command));
+		executeWithRetry(MAX_OPTIMISTIC_RETRY_COUNT, () -> decreaseAvailabilityCount(command));
 
 		// 4. 총 숙박 요금 계산
 		int totalPrice = calculateTotalPrice(command);
@@ -121,5 +131,84 @@ public class ReservationService {
 
 		// 버전 기반 낙관적 락으로 saveAll 시점에 충돌 검증
 		return roomAvailabilityRepository.saveAll(availabilities);
+	}
+
+	/**
+	 * Redisson을 사용하여 예약 생성
+	 * - 회원 존재 여부 확인
+	 * - 룸 타입 존재 여부 확인
+	 * - 예약 가능 여부 확인
+	 * - 수량 차감 (Redisson Lock)
+	 * - 총 숙박 요금 계산
+	 * - 임시 예약(PENDING) 생성
+	 */
+	public CreateReservationResult redissonCreateReservation(long memberId, CreateReservationCommand command) {
+		// 0. 회원 존재 여부 확인
+		Member member = memberRepository.findById(memberId)
+			.orElseThrow(() -> ErrorCode.NOT_FOUND.exception("회원이 존재하지 않습니다."));
+
+		// 1. 예약 룸 정보 조회
+		RoomType roomType = roomTypeRepository.findById(command.roomTypeId())
+			.orElseThrow(() -> ErrorCode.NOT_FOUND.exception("룸 타입이 존재하지 않습니다."));
+
+		// 2. 비관적 락 처리 시작
+		RLock lock = getReservationLock(command);
+
+		boolean isLocked = false;
+		try {
+			isLocked = lock.tryLock(MAX_LOCK_WAIT_TIME_SECONDS, LOCK_WAIT_TIME_SECONDS, TimeUnit.SECONDS);
+
+			if (!isLocked) {
+				throw ErrorCode.CONFLICT.exception("해당 객실은 현재 다른 예약 처리 중입니다. 잠시 후 다시 시도해 주세요.");
+			}
+
+			// 3. 예약 가능 여부 확인
+			validateAvailability(command);
+
+			// 4. 수량 차감
+			decreaseAvailabilityCount(command);
+
+			// 5. 총 숙박 요금 계산
+			int totalPrice = calculateTotalPrice(command);
+
+			// 6. 임시 예약(PENDING) 생성
+			Reservation reservation = reservationRepository.save(
+				Reservation.builder()
+					.roomTypeId(command.roomTypeId())
+					.memberId(member.getId())
+					.checkIn(command.checkIn())
+					.checkOut(command.checkOut())
+					.guestCount(command.guestCount())
+					.phoneNumber(member.getPhoneNumber())
+					.totalPrice(totalPrice)
+					.status(ReservationStatus.PENDING)
+					.build()
+			);
+
+			return new CreateReservationResult(
+				reservation.getId(),
+				member.getEmail(),
+				member.getPhoneNumber(),
+				roomType.getName(),
+				reservation.getStatus(),
+				reservation.getTotalPrice(),
+				impUid
+			);
+
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			throw ErrorCode.INTERNAL_SERVER_ERROR.exception("예약 처리 중 오류 발생");
+		} finally {
+			if (isLocked) {
+				lock.unlock();
+			}
+		}
+	}
+
+	private RLock getReservationLock(CreateReservationCommand command) {
+		String lockKey =
+			"reservation:lock:" + command.roomTypeId() + ":" + command.checkIn() + ":" + command.checkOut();
+
+		return redisson.getLock(lockKey);
 	}
 }
