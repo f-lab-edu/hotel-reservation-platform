14a567a0e4a2996a0e7a744433b6cbd46573bb7e
Pablo730 <kcm1619@naver.com>
2025-09-05 09:07:13 +0900

    fix: 비밀번호 검증 정규식 상수명 수정

--- identity-service/src/main/kotlin/com/msa/identityservice/member/controller/request/MemberRegistrationRequest.kt
+++ identity-service/src/main/kotlin/com/msa/identityservice/member/controller/request/MemberRegistrationRequest.kt
@@ -11,7 +11,7 @@ data class MemberRegistrationRequest(
     val email: String,
 
     @field:NotBlank(message = "비밀번호는 필수입니다.")
-    @field:Pattern(regexp = PASSWORD_PATTERN,message = "비밀번호는 8자 이상이며, 영문, 숫자, 특수문자를 포함해야 합니다.")
+    @field:Pattern(regexp = PW_PATTERN,message = "비밀번호는 8자 이상이며, 영문, 숫자, 특수문자를 포함해야 합니다.")
     val password: String,
 
     @field:NotBlank(message = "휴대폰 번호는 필수입니다.")
@@ -28,7 +28,7 @@ data class MemberRegistrationRequest(
 
     companion object {
         // 비밀번호 정책 검증 정규식 (최소 8자, 영문, 숫자, 특수문자 각 1개 이상 포함)
-        private const val PASSWORD_PATTERN = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$"
+        private const val PW_PATTERN = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$"
 
         // 대한민국 휴대폰 번호 형식 검증 정규식 (010-XXXX-XXXX)
         private const val PHONE_NUMBER_PATTERN = "^010-\\d{4}-\\d{4}$"
