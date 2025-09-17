0dedac8f7fc864a6c4783c9f21c41aac3b3151d4
Pablo730 <kcm1619@naver.com>
2025-09-18 03:58:38 +0900

    fix: 불필요한 import 제거

--- services/auth/src/main/kotlin/msa/hotel/services/auth/config/SecurityConfig.kt
+++ services/auth/src/main/kotlin/msa/hotel/services/auth/config/SecurityConfig.kt
@@ -2,7 +2,6 @@ package msa.hotel.services.auth.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
